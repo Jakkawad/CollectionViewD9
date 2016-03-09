198788f4697b1cf6250cbbdb5e7a9fb39555dcf8
Jakkawad <admin@all2sale-iMac.local>
2016-03-09 12:28:28 +0700

    V3
    V3

--- CollectionViewD9/CustomTableViewCell.swift
+++ CollectionViewD9/CustomTableViewCell.swift
@@ -7,22 +7,42 @@
 //
 
 import UIKit
+import Alamofire
 
 class CustomTableViewCell: UITableViewCell, UICollectionViewDataSource, UICollectionViewDelegate {
 
     @IBOutlet weak var lblName: UILabel!
-    @IBOutlet weak var mainCollectionView: CustomCollectionViewCell!
+   
+    @IBOutlet weak var mainCollectionView: UICollectionView!
+    
+    var mainDataArray = NSArray()
+    
+    func feedData() {
+        let params = ["api":"productall","productall":"37"]
+        Alamofire.request(.POST, "https://www.all2sale.com/sendmail/testfunction/json/apitest.php", parameters: params, encoding: .URL, headers: nil).responseJSON { (request, response, result) -> Void in
+            self.mainDataArray = result.value as! NSArray
+            //print(self.mainDataArray.description)
+            self.mainCollectionView.reloadData()
+        }   //print(result.value!)
+        
+        
+    }
+
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        //return 10
+        return self.mainDataArray.count
     }
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let col1 = collectionView.dequeueReusableCellWithReuseIdentifier("collectView1", forIndexPath: indexPath) as? CustomCollectionViewCell
-        
+        let item = self.mainDataArray[indexPath.row] as! NSDictionary
+
+        col1?.lblTitle.text = item.objectForKey("ProductName") as? String
         return col1!
     }
     override func awakeFromNib() {
         super.awakeFromNib()
+        feedData()
         // Initialization code
     }
 
